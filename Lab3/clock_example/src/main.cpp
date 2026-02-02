dac8fa3d10a138ffd72bc5928519360d5b2c5de6
KVo5657 <67343334+KVo5657@users.noreply.github.com>
2026-02-01 19:30:33 -0800

    Lab3 Done

---
+++ Lab3/clock_example/src/main.cpp
@@ -0,0 +1,56 @@
+//
+//  main.cpp
+//  Lab4
+//
+//  Created by Alireza on 2/14/20.
+//  Copyright Â© 2020 Alireza. All rights reserved.
+//
+
+#include "main.h"
+#include "cycletime.h"
+#include "timer.h"
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+	float cpu_timer;
+	unsigned int delay = 1;
+	
+	cout << "WES237A lab 4" << endl;
+
+	char key=0;
+	
+	// 1 argument on command line: delay = arg
+	if(argc >= 2)
+	{
+		delay = atoi(argv[1]);
+}
+
+    //TODO: declare 2 cpu_count variables: 1 for before sleeping, 1 for after sleeping (see cpu_timer)
+    unsigned int cpu_count_before, cpu_count_after, cpu_count;
+    
+    //TODO: initialize the counter
+    init_counters(1, 0);
+    
+    //TODO: get the cyclecount before sleeping
+    cpu_count_before = get_cyclecount();
+    usleep(delay);
+    //TODO: get the cyclecount after sleeping
+    cpu_count_after = get_cyclecount();
+
+    //TODO: subtract the before and after cyclecount
+    cpu_count = cpu_count_after - cpu_count_before;
+    
+	LinuxTimer t;
+	usleep(delay);
+	t.stop();
+	cpu_timer = t.getElapsed();
+
+	//TODO: print the cycle count (see the print statement for the cpu_timer below)
+	cout << "CPU Cycle Count: " << cpu_count << "\n" << "Timer: " << (double)cpu_timer/1000000000.0 << endl;
+
+	return 0;
+}
+
